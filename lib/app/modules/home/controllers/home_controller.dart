8c70030411c3e3f7384d5f609aabed6cb8610630
adilagiska05 <adila_074@smkassalaambandung.sch.id>
2023-02-23 15:54:16 +0700

    tahap 2 & 3

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,33 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
+import 'package:mobile_project/app/modules/login/views/login_view.dart';
 
 class HomeController extends GetxController {
   //TODO: Implement HomeController
+  late Timer _pindah;
 
-  final count = 0.obs;
+
   @override
   void onInit() {
+    _pindah = Timer.periodic(
+      const Duration(seconds: 4),
+      (timer) => Get.off(
+        () =>  LoginView(),
+        transition: Transition.leftToRight,
+      ),
+    );
     super.onInit();
+
   }
 
   @override
   void onReady() {
+    _pindah.cancel();
     super.onReady();
   }
 
   @override
   void onClose() {}
-  void increment() => count.value++;
+ 
 }
