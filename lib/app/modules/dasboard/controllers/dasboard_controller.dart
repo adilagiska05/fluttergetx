cc931bb33c9ff9f33e6fac258ff7fa3cf3e1ac17
adilagiska05 <adila_074@smkassalaambandung.sch.id>
2023-02-25 08:34:38 +0700

    tahap 5

--- lib/app/modules/dasboard/controllers/dasboard_controller.dart
+++ lib/app/modules/dasboard/controllers/dasboard_controller.dart
@@ -1,9 +1,38 @@
+import 'dart:convert';
+
 import 'package:get/get.dart';
+import 'package:mobile_project/app/data/entertainment_response.dart';
+import 'package:mobile_project/app/data/headline_response.dart';
+import 'package:mobile_project/app/data/sports_response.dart';
+import 'package:mobile_project/app/data/technology_response.dart';
+import 'package:mobile_project/app/utils/api.dart';
 
 class DasboardController extends GetxController {
   //TODO: Implement DasboardController
 
   final _getConnect = GetConnect();
+
+  Future<HeadlineResponse> getHeadline() async {
+  final response = await _getConnect.get(BaseUrl.headline);
+  return HeadlineResponse.fromJson(jsonDecode(response.body));
+}
+
+Future<EntertainmentResponse> getEntertainment() async {
+  final response = await _getConnect.get(BaseUrl.entertainment);
+  return EntertainmentResponse.fromJson(jsonDecode(response.body));
+}
+
+
+Future<SportsResponse> getSports() async {
+  final response = await _getConnect.get(BaseUrl.sports);
+  return SportsResponse.fromJson(jsonDecode(response.body));
+}
+
+
+Future<TechnologyResponse> getTechnology() async {
+  final response = await _getConnect.get(BaseUrl.technology);
+  return TechnologyResponse.fromJson(jsonDecode(response.body));
+}
 
   final count = 0.obs;
   @override
